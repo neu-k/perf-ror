c484ed642693fec938ee3c16cd02c3331e7a133f
neu-k <rsk367277@gmail.com>
2024-10-06 02:08:47 +0900

    rails g model user name:string address:string email:string birthday:date

--- Library/db/schema.rb
+++ Library/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_30_205725) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_05_170351) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
+    t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
